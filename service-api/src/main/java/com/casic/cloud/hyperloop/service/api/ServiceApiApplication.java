09640f00a7945d9194829efd33451a00cb6b941a
AnglIng <jack19880707@126.com>
2020-02-22 22:45:17 +0800

    service api 临时增加端口配置等 后续考虑是否做成纯jar包

--- service-api/src/main/java/com/casic/cloud/hyperloop/service/api/ServiceApiApplication.java
+++ service-api/src/main/java/com/casic/cloud/hyperloop/service/api/ServiceApiApplication.java
@@ -3,9 +3,10 @@ package com.casic.cloud.hyperloop.service.api;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
 @MapperScan("com.casic.cloud.hyperloop.service.api.dao")
 @ComponentScan(basePackages = {"com.casic.cloud.hyperloop"})
 public class ServiceApiApplication {

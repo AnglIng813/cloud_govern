ca97c3186f0abad24df4b858a038c56711f22923
liuyuhao <liu233511@163.com>
2020-03-09 10:43:34 +0800

    限流配置修改

--- core/src/main/resources/static/js/controller/limit-set.js
+++ core/src/main/resources/static/js/controller/limit-set.js
@@ -1,4 +1,14 @@
 angular.module('app').controller("limit-set", function($scope, $http, $state, $rootScope, $timeout) {
   $scope.getP = 1;
   $scope.newP = 1;
+
+  // 配置分页基本参数
+  $scope.paginationConf = {
+    currentPage: sessionStorage.currentPage || 1,
+    totalItems: 8000,
+    itemsPerPage: 10,
+    pagesLength: 5, //页码
+    perPageOptions: [10, 25, 50, 100],
+    onChange: function() {}
+  };
 })
